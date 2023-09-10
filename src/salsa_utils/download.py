4833d2d418353ca9d92a71ece9ce972464463233
alinacherkas <51997505+alinacherkas@users.noreply.github.com>
2023-09-10 14:53:11 +0200

    refactor(downloads): require to pass annotations explicitly

--- src/salsa_utils/download.py
+++ src/salsa_utils/download.py
@@ -5,15 +5,11 @@ from urllib.request import urlretrieve
 # utils
 from tqdm import tqdm
 
-# salsa
-from .read import read_annotations
 
-
-def download_images(output_dir: str) -> bool:
+def download_images(annotations: dict, output_dir: str) -> bool:
     if os.path.exists(output_dir):
         raise Exception('Directory already exists.')
     os.mkdir(output_dir)
-    annotations = read_annotations()
     for image in tqdm(annotations['images']):
         _, file_name = os.path.split(image['flickr_url'])
         file_path = os.path.join(output_dir, file_name)
