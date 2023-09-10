be3dc5c8f9b377bd9dac776723544ccbf59e961d
alinacherkas <51997505+alinacherkas@users.noreply.github.com>
2023-09-10 14:53:39 +0200

    feat: add salsa module to create the dataset in yolo format

--- src/salsa_utils/__init__.py
+++ src/salsa_utils/__init__.py
@@ -1,4 +1,5 @@
 from .read import read_annotations
 from .download import download_images
 from .transform import convert_to_yolo
-from .write import write_splits, write_labels
+from .write import write_splits, write_labels, write_yaml
+from .salsa import prepare_salsa
